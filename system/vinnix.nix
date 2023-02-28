5461746c0d4ab9d1324fcab2cd754ae9a9e5fb48
Vinny Meller <vinnymeller@gmail.com>
2023-02-28 02:48:44 -0600

    add more helper stuff

--- system/vinnix.nix
+++ system/vinnix.nix
@@ -79,18 +79,17 @@
     zsh
     wget
     firefox
+    looking-glass-client
+    spice
+    zoom-us
+  ];
+
+  systemd.tmpfiles.rules = [
+    "f /dev/shm/looking-glass 0660 user kvm -" # looking-glass shmem
   ];
 
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemu.ovmf.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   system.stateVersion = "22.11"; # read documentation on configuration.nix before possibly changing this
 
