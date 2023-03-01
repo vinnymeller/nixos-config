d99ac0b3bb7e665e28e623051acab0a8077088d9
Vinny Meller <vinnymeller@gmail.com>
2023-03-01 13:03:31 -0600

    get rid of gnome wtf was that?

--- system/vinnix.nix
+++ system/vinnix.nix
@@ -61,8 +61,7 @@
   # Setup X11
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.autoRepeatDelay = 200;
   services.xserver.autoRepeatInterval = 45;
   services.xserver.autorun = true;
