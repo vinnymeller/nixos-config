df968bb7530b8c560c540bfaef6322734a0f3e1a
Vinny Meller <vinnymeller@gmail.com>
2023-03-02 03:18:50 -0600

    move zsh config to own file

--- users/vinny/home.nix
+++ users/vinny/home.nix
@@ -31,55 +31,50 @@
     zsh-powerlevel10k
   ];
 
-  # programs.git = {
-  #   enable = true;
-  #   userName = "Vinny Meller";
-  #   userEmail = "vinnymeller@gmail.com";
-  # };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    dotDir = ".config/zsh";
-
-
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-
-    initExtra = "source ${config.xdg.configHome}/zsh/.p10k.zsh\n";
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" ];
-    };
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-	src = pkgs.fetchFromGitHub {
-	  owner = "zsh-users";
-	  repo = "zsh-autosuggestions";
-	  rev = "v0.7.0";
-	  sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-	};
-      }
-      {
-        name = "zsh-syntax-highlighting";
-	src = pkgs.fetchFromGitHub {
-	  owner = "zsh-users";
-	  repo = "zsh-syntax-highlighting";
-	  rev = "0.7.1";
-	  sha256 = "gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
-	};
-      }
-      {
-        name = "powerlevel10k";
-	src = pkgs.zsh-powerlevel10k;
-	file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
-  };
+ #  programs.zsh = {
+ #    enable = true;
+ #    enableCompletion = true;
+ #    dotDir = ".config/zsh";
+	#
+	#
+ #    history = {
+ #      size = 10000;
+ #      path = "${config.xdg.dataHome}/zsh/history";
+ #    };
+	#
+ #    initExtra = "source ${config.xdg.configHome}/zsh/.p10k.zsh\n";
+	#
+ #    oh-my-zsh = {
+ #      enable = true;
+ #      plugins = [ "git" ];
+ #    };
+ #    plugins = [
+ #      {
+ #        name = "zsh-autosuggestions";
+	# src = pkgs.fetchFromGitHub {
+	#   owner = "zsh-users";
+	#   repo = "zsh-autosuggestions";
+	#   rev = "v0.7.0";
+	#   sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+	# };
+ #      }
+ #      {
+ #        name = "zsh-syntax-highlighting";
+	# src = pkgs.fetchFromGitHub {
+	#   owner = "zsh-users";
+	#   repo = "zsh-syntax-highlighting";
+	#   rev = "0.7.1";
+	#   sha256 = "gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
+	# };
+ #      }
+ #      {
+ #        name = "powerlevel10k";
+	# src = pkgs.zsh-powerlevel10k;
+	# file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+ #      }
+ #    ];
+ #  };
 
   programs.kitty = {
     enable = true;
