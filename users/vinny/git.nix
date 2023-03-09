160e07be1597050a5d5fc9f108e3ef8ef3fef721
Vinny Meller <vinnymeller@gmail.com>
2023-03-08 19:30:34 -0600

    add extra git config

--- users/vinny/git.nix
+++ users/vinny/git.nix
@@ -3,5 +3,11 @@
         enable = true;
         userName = "Vinny Meller";
         userEmail = "vinnymeller@gmail.com";
+        extraConfig = {
+            push.autoSetupRemote = true;
+            push.default = "simple";
+            pull.rebase = true;
+            core.editor = "nvim";
         };
-    }
+    };
+}
