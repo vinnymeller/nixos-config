c233c6ab03db882ebd73a7a670118f8a5b45b37e
Vinny Meller <vinnymeller@gmail.com>
2023-03-18 02:07:25 -0500

    change kitty so stop fkin asking me if i want to close window

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -6,6 +6,10 @@
             name = "Jetbrains Mono";
             package = pkgs.jetbrains-mono;
         };
-        extraConfig = "background_opacity   0.85";
+
+        extraConfig = ''
+        background_opacity   0.85
+        confirm_os_window_close 0
+        '';
     };
 }
