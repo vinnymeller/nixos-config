630cdb302295dc753637755ebbd2b5ef033dcd41
Vinny Meller <vinnymeller@gmail.com>
2023-04-11 19:48:19 -0500

    get rid of shitty logout button

--- dotfiles/qtile/bars.py
+++ dotfiles/qtile/bars.py
@@ -19,6 +19,5 @@ def get_bar(main_bar=False):
         widget.Memory(measure_mem="G"),
         widget.Systray() if main_bar else widget.Sep(),
         widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-        widget.QuickExit(default_text="[  ]"),
     ]
     return bar.Bar(bar_items, 24, border_width=0)
