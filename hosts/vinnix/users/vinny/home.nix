2ac898ee985b72bc1491d376b39051a8850328c0
Vinny Meller <vinnymeller@gmail.com>
2023-07-06 11:45:41 -0500

    add mullvad and popcorn time

--- hosts/vinnix/users/vinny/home.nix
+++ hosts/vinnix/users/vinny/home.nix
@@ -42,11 +42,13 @@ in
     libvirt
     lxde.lxsession
     man-pages # linux man pages, goes with tlpi
+    mullvad-vpn
     neofetch
     nerdfonts
     obs-studio
     pavucontrol
     pkg-config
+    popcorntime
     protonup-qt
     qemu
     rofi
