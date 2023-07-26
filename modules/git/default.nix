00a250124d4b92c842220c643a3e0ff704e44fe8
Vinny Meller <vinnymeller@gmail.com>
2023-07-26 10:24:29 -0500

    test

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -9,5 +9,13 @@
             pull.rebase = true;
             core.editor = "nvim";
         };
+        aliases = {
+            pall = "!f() { git commit -am \"$1\" && git push; }; f";
+            cob = "checkout -b";
+            del = "branch -D";
+            br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+            lg = "!git log --pretty=format:\"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\" --abbrev-commit -30";
+        };
+
     };
 }
