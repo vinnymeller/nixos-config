f06cca3b77eee3d95dbe30f84d4ba01215a148a8
Vinny Meller <vinnymeller@gmail.com>
2023-08-16 01:25:12 -0500

    update keymaps for some fast git stuff

--- dotfiles/nvim/lua/keymappings.lua
+++ dotfiles/nvim/lua/keymappings.lua
@@ -45,3 +45,8 @@ map("n", "<C-Shift-k>", ":echo 'hi'<CR>", { desc = "Next buffer" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "Move [H]orizontal to window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move [L]over to window right" })
+
+
+-- stage current file in git
+map("n", "<leader>gs", "<cmd>Git add %<CR>", { desc = "[G]it [S]tage current file" })
+map("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "[G]it [C]ommit" })
