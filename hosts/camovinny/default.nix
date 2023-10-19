54f0440edaa309432c4cc4394fd5a23023a17d7e
Vinny Meller <vinnymeller@proton.me>
2023-10-18 23:22:12 -0400

    idk

--- hosts/camovinny/default.nix
+++ hosts/camovinny/default.nix
@@ -1,12 +1,5 @@
 { inputs, outputs, ... }:
 inputs.home-manager.lib.homeManagerConfiguration {
-<<<<<<< HEAD
-  pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-  extraSpecialArgs = { inherit inputs outputs; };
-  modules = [
-    ./home.nix
-  ];
-=======
     pkgs = import inputs.nixpkgs {
         system = "aarch64-darwin";
         config = {
@@ -17,5 +10,4 @@ inputs.home-manager.lib.homeManagerConfiguration {
     modules = [
         ./home.nix
     ];
->>>>>>> 8e08d34 (add laptop changes)
 }
