4d5a79512646871d60656604ea0a60d96414dbee
Vinny Meller <vinnymeller@proton.me>
2023-12-13 11:21:09 -0500

    Update vinnix to use shared nix config

--- hosts/vinnix/configuration.nix
+++ hosts/vinnix/configuration.nix
@@ -6,29 +6,10 @@
     ../../programs/gpg
   ];
 
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      auto-optimise-store = true;
-      substituters = [
-      	"https://nix-community.cachix.org"
-	"https://cache.nixos.org"
-      ];
-      trusted-public-keys = [
-      	"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 14d";
-    };
-  };
-
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
-      permittedInsecurePackages = [ "electron-12.2.3" ];
-      allowUnfree = true;
+      permittedInsecurePackages = [ "electron-25.9.0" ];
     };
   };
 
--- programs/nix/default.nix
+++ programs/nix/default.nix
@@ -10,8 +10,6 @@
       trusted-public-keys = [
       	"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      trusted-users = [ "vinny" "root" ];
-      require-sigs = false;
     };
     gc = {
       automatic = true;
