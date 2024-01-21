a027f3a7c1331cd89e115836bbe7fbccd5dfe986
Vinny Meller <vinnymeller@proton.me>
2024-01-21 03:22:15 -0500

    yk

--- dotfiles/xrandr_layout.sh
+++ dotfiles/xrandr_layout.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# come back to trying to get fractional scaling working nicely
+# source of the scaling stuff: https://web.archive.org/web/20240121070644/https://wilfredwee.github.io/entry/how-to-xrandr
+# I like how 2k looks on a 27", 4k on 32" is too small still, 4k is 1.5x each dimension of 2k, 1.25x instead of 1.5x gives 3200x1800 and seems pretty good. so I want my perceived resolution on a 4k monitor to be 3200x1800
 xrandr \
     --output HDMI-0 --off \
     --output DP-0 --off \
