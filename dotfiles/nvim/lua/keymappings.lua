4c9835773ba0d21734574e915f9a9c61ea7ebfe4
Vinny Meller <vinnymeller@proton.me>
2024-04-27 03:04:48 -0400

    update some keymappies, fugitive mainly. delete ones i dont want to keep using

--- dotfiles/nvim/lua/keymappings.lua
+++ dotfiles/nvim/lua/keymappings.lua
@@ -23,29 +23,9 @@ map("v", "<", "<gv", { noremap = true })
 -- backspace in normal mode to replace current word. can i upgrade this ? think about it
 map("n", "<bs>", "ciw", { desc = "Replace current word" })
 
-map("n", "<leader>sv", "<cmd>vs<CR>", { desc = "[S]plit [V]ertical" })
-map("n", "<leader>sh", "<cmd>sp<CR>", { desc = "[S]plit [H]orizontal" })
-
 -- exit window & close buffer
 map("n", "<leader>q", "<cmd>q<CR>", { desc = "[Q]uit" })
 map("n", "<leader>k", "<cmd>bd<CR>", { desc = "[K]ill" })
-
-map("n", "<C-j>", ":bprev<CR>", { desc = "Previous buffer" })
-map("n", "<C-k>", ":bnext<CR>", { desc = "Next buffer" })
-
--- stage current file in git
-map("n", "<leader>gs", "<cmd>Git add %<CR>", { desc = "[G]it [S]tage current file" })
-map("n", "<leader>gS", "<cmd>Git add -A<CR>", { desc = "[G]it [S]tage all files" })
-map("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "[G]it [C]ommit" })
-map("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "[G]it [P]ush" })
-map("n", "<leader>gd", "<cmd>Git diff %<CR>", { desc = "[G]it [D]iff current file" })
-map("n", "<leader>gD", "<cmd>Git diff<CR>", { desc = "[G]it [D]iff" })
-map("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "[G]it [B]lame" })
-map("n", "<leader>gl", "<cmd>Git log<CR>", { desc = "[G]it [L]og" })
-map("n", "<leader>g<C-s>", "<cmd>Git status<CR>", { desc = "[G]it [S]tatus" })
-map("n", "<leader>gr", "<cmd>Git restore %<CR>", { desc = "[G]it [R]estore current file" })
-map("n", "<leader>gR", "<cmd>Git restore .<CR>", { desc = "[G]it [R]estore all files" })
-map("n", "<leader>gP", "<cmd>Git pull<CR>", { desc = "[G]it [P]ull" })
 
 -- open dadbod ui
 map("n", "<leader>db", "<cmd>DBUIToggle<CR>", { desc = "[D]ad[B]od Toggle" })
