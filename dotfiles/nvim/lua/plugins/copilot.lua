15dae0332444466a08d059832986b08f392846fc
Vinny Meller <vinnymeller@proton.me>
2024-05-21 00:58:36 -0400

    don't disable copilot for some filetypes

--- dotfiles/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,11 @@
 require("copilot").setup({
 	panel = { enabled = false },
 	suggestion = { enabled = false },
+	-- enable all filetypes
+	filetypes = {
+		markdown = true,
+		yaml = true,
+		gitcommit = true,
+		gitrebase = true,
+	},
 })
