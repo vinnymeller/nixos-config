d60ab613e7efe2c30fa7e93495bd6cb08222b0aa
Vinny Meller <vinnymeller@proton.me>
2024-06-01 17:06:04 -0400

    move wallpaper to lfs in this repo

--- programs/qtile/default.nix
+++ programs/qtile/default.nix
@@ -24,7 +24,7 @@
         ~/.nixdots/dotfiles/xrandr_layout.sh
         xset dpms 0 0 0 && xset s noblank && xset s off
         lxsession -s qtile -e qtile &
-        feh --bg-fill ~/Downloads/wp7009163-avatar-appa-wallpapers.png
+        feh --bg-fill ~/.nixdots/files/avatar-wallpaper.png
       '';
     };
     windowManager.qtile = {
