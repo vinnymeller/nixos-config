e8d5e91cdc09df3a6a0de4f3fa2262281646c14c
Vinny Meller <vinnymeller@proton.me>
2024-06-03 02:59:09 -0400

    add twmg alias to fork & clone a repo & open it in twm

--- programs/git/default.nix
+++ programs/git/default.nix
@@ -66,6 +66,7 @@
 
   home.shellAliases = {
     fork = "gh repo fork --clone --default-branch-only --remote";
+    twmg = "twmg() { fork \"$1\" \"$2\"; twm -p \"$2\"; }; twmg";
     gs = "git swap"; # depends on the git alias above
   };
 }
