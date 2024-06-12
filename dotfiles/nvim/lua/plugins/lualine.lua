02208898a861e7fa3f4f53dd89b448be4956eea0
Vinny Meller <vinnymeller@proton.me>
2024-06-12 17:27:30 -0400

    update lualine config to show relative filepath instead of just name

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,7 @@
 require("lualine").setup({
-    options = {
-        icons_enabled = true,
-        theme = "gruvbox",
-        -- component_separators = "|",
-        -- section_separators = "",
-    }
+	options = {
+		icons_enabled = true,
+		theme = "gruvbox",
+		path = 1,
+	},
 })
