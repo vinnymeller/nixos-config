83fb99dbff55399c3f718ff9652195fb33e5f5b7
Vinny Meller <vinnymeller@proton.me>
2024-06-19 23:33:48 -0400

    revert oil settings

--- dotfiles/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/lua/plugins/oil.lua
@@ -4,8 +4,5 @@ require("oil").setup({
 	view_options = {
 		show_hidden = true,
 	},
-	lsp_file_methods = {
-		autosave_changes = true,
-	},
 })
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
