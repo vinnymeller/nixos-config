5081074762469a6d941f1aaa30f71ebbad0dde19
Vinny Meller <vinnymeller@proton.me>
2024-08-26 03:37:39 -0400

    force clean git tree to rebuild

--- hosts/vinnix/configuration.nix
+++ hosts/vinnix/configuration.nix
@@ -221,6 +221,14 @@
   };
   hardware.gpgSmartcards.enable = true; # for yubikey
 
+  # Tag each generation with Git hash 
+  system.configurationRevision =
+    if (inputs.self ? rev) then
+      inputs.self.shortRev
+    else
+      throw "Refusing to build from a dirty Git tree!";
+  system.nixos.label = "GitRev.${config.system.configurationRevision}.Rel.${config.system.nixos.release}";
+
   programs.command-not-found.enable = false;
 
   programs.nm-applet.enable = true;
