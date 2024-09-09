2f4eee9558274cb3d089ea71d065c34b07420271
Vinny Meller <vinnymeller@proton.me>
2024-09-09 19:26:16 -0400

    add prev/next hunk keybinds

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -83,3 +83,10 @@ require("nvim-treesitter.configs").setup({
 		},
 	},
 })
+
+local ts_repeat_move = require ("nvim-treesitter.textobjects.repeatable_move")
+local gs = require("gitsigns")
+
+local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
+vim.keymap.set({ "n", "x", "o" }, "]h", next_hunk_repeat, { desc = "Next Git hunk" })
+vim.keymap.set({ "n", "x", "o" }, "[h", prev_hunk_repeat, { desc = "Previous Git hunk" })
