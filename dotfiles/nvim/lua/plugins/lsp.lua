f4def8fc3a8ae3954d3c6e79c44ef09a7a99be90
Vinny Meller <vinnymeller@proton.me>
2024-11-08 13:20:04 -0500

    update blink config

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -59,23 +59,67 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 require("blink.compat").setup({})
 require("blink.cmp").setup({
+	accept = {
+		auto_brackets = {
+			enabled = true,
+		},
+	},
+	fuzzy = {
+		prebuilt_binaries = {
+			download = false,
+		},
+	},
 	highlight = {
 		use_nvim_cmp_as_default = true,
+	},
+	keymap = {
+		["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+		["<C-e>"] = { "hide", "fallback" },
+
+		["<Tab>"] = {
+			function(cmp)
+				if cmp.is_in_snippet() then
+					return cmp.accept()
+				else
+					return cmp.select_and_accept()
+				end
+			end,
+			"snippet_forward",
+			"fallback",
+		},
+		["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+		["<Up>"] = { "select_prev", "fallback" },
+		["<Down>"] = { "select_next", "fallback" },
+		["<C-p>"] = { "select_prev", "fallback" },
+		["<C-n>"] = { "select_next", "fallback" },
+		["<C-y>"] = { "select_and_accept" },
+
+		["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		["<C-f>"] = { "scroll_documentation_down", "fallback" },
 	},
 	trigger = {
 		signature_help = {
 			enabled = true,
 		},
 		completion = {
-			show_in_snippet = false,
+			show_in_snippet = true,
 		},
 	},
 	windows = {
+		autocomplete = {
+			draw = {
+				columns = {
+					{ "label", "label_description", gap = 1 },
+					{ "kind_icon" },
+				},
+			},
+		},
 		documentation = {
 			auto_show = true,
 		},
 		ghost_text = {
-			enabled = true,
+			enabled = false,
 		},
 	},
 	sources = {
