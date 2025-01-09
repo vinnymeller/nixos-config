8552dc1beca87664b5083586b6397fe8bf57e614
Vinny Meller <vinnymeller@proton.me>
2025-01-09 17:41:32 -0500

    revert to working nvim

--- dotfiles/nvim/lua/keymappings.lua
+++ dotfiles/nvim/lua/keymappings.lua
@@ -5,7 +5,6 @@ map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map("i", "jk", "<Esc>", { desc = "jk to escape" })
-map("i", "kj", "<Esc>", { desc = "kj to escape" })
 
 -- Diagnostic keymaps
 map("n", "<leader>e", vim.diagnostic.open_float)
