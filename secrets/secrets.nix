26f58e61b4b64c2a0a25f25a64c900d9ac29d57e
Vinny Meller <vinnymeller@proton.me>
2025-02-17 17:03:23 -0500

    add keys for agenix, TODO still need rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,17 @@
 let
   vindows-vinny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILL1ICSjnFqxiAFubcpOSZN1vuNo/w4sTJoWXpfYnhoq";
-  users = [ vindows-vinny ];
+  vinnix-vinny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOh+L9Lv8ai9K6q26k9rqUmnYorThf1g4FTUAS6NGws";
+  users = [
+    vindows-vinny
+    vinnix-vinny
+  ];
 
   vindows = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFfKSa6jvWn/K+jXzntb1AGHbhfLGsjbS50U5kwjPsR";
-  systems = [ vindows ];
+  vinnix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYDnYYFjCnSjyBb6ijuqNX6zLb/ItHQRm7MY9toHO8B";
+  systems = [
+    vindows
+    vinnix
+  ];
 in
 {
   "github-nix-ci/mxves.token.age".publicKeys = users ++ systems;
