1ed1df5df26fbed00019e9dcf135443ca3c2c829
Vinny Meller <vinnymeller@proton.me>
2025-02-19 07:28:53 +0000

    idk

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,9 @@
 let
   vindows-vinny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILL1ICSjnFqxiAFubcpOSZN1vuNo/w4sTJoWXpfYnhoq";
   vinnix-vinny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOh+L9Lv8ai9K6q26k9rqUmnYorThf1g4FTUAS6NGws";
-  camo-vinny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJD0xnm6ozJiSEPX6ot9qADGPPXWuePO+kjwTM3RYgWL";
   users = [
     vindows-vinny
     vinnix-vinny
-    camo-vinny
   ];
 
   vindows = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFfKSa6jvWn/K+jXzntb1AGHbhfLGsjbS50U5kwjPsR";
@@ -16,10 +14,7 @@ let
   ];
 in
 {
-  "github-nix-ci/mxves.token.age".publicKeys = [
-    vindows-vinny
-    vinnix-vinny
-  ] ++ systems;
-  "cloudflared/moves/credentials.json.age".publicKeys = systems;
+  "github-nix-ci/mxves.token.age".publicKeys = users ++ systems;
+  "cloudflared/moves/credentials.json.age".publicKeys = users ++ systems;
   "shell/secrets.sh.age".publicKeys = users ++ systems;
 }
