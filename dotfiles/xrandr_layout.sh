5dad752dcd0cea2d99516c22f1d04f96b89c1880
Vinny Meller <vinnymeller@proton.me>
2025-07-02 02:48:18 -0400

    update xrandr settings

--- dotfiles/xrandr_layout.sh
+++ dotfiles/xrandr_layout.sh
@@ -4,11 +4,11 @@
 # I like how 2k looks on a 27", 4k on 32" is too small still, 4k is 1.5x each dimension of 2k, 1.25x instead of 1.5x gives 3200x1800 and seems pretty good. so I want my perceived resolution on a 4k monitor to be 3200x1800
 xrandr \
     --output HDMI-0 --off \
-    --output DP-0 --off \
+    --output DP-0 --mode 3840x2160 --pos 7680x0 --rotate right --rate 60.00 \
     --output DP-1 --off \
     --output DP-2 --primary --mode 7680x2160 --pos 0x909 --rotate normal --rate 120.00 \
     --output DP-3 --off \
-    --output DP-4 --mode 3840x2160 --pos 7680x0 --rotate right --rate 60.00 \
+    --output DP-4 --off \
     --output DP-5 --off
 
 # split virtual monitors for screen sharing
