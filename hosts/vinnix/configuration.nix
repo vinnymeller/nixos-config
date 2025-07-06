dc58062f20f252f38977fdf62580741148ad1eb5
Vinny Meller <vinnymeller@proton.me>
2025-07-06 02:51:38 -0400

    use vt 2 for greetd

--- hosts/vinnix/configuration.nix
+++ hosts/vinnix/configuration.nix
@@ -248,6 +248,7 @@
   };
   services.greetd = {
     enable = true;
+    vt = 2;
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd hyprland";
