040864cca4511d874c968e87e631deba08ec7137
Vinny Meller <vinnymeller@proton.me>
2025-07-23 18:13:48 -0400

    nice binds

--- dotfiles/nvim/lua/plugins/gitsigns.lua
+++ dotfiles/nvim/lua/plugins/gitsigns.lua
@@ -1,10 +1,10 @@
 -- TODO: uncomment gitsigns every so often to see if it fuckin works yet because im too lazy to debug the issue
--- require("gitsigns").setup({
---     signs = {
---         add = { text = "+" },
---         change = { text = "~" },
---         delete = { text = "_" },
---         topdelete = { text = "‾" },
---         changedelete = { text = "~" },
---     },
--- })
+require("gitsigns").setup({
+    signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+    },
+})
