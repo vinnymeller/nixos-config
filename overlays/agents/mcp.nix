76467a5c0cd50cca7ddf3f4c0202528d6f064f75
Vinny Meller <vinnymeller@proton.me>
2026-01-28 10:34:06 -0600

    dont include chrome mcp on aarch64-linux

--- overlays/agents/mcp.nix
+++ overlays/agents/mcp.nix
@@ -1,18 +1,10 @@
 { pkgs }:
 let
+  inherit (pkgs) lib;
   npx = "${pkgs.nodejs}/bin/npx";
   codex = "${pkgs.codex}/bin/codex";
 in
 {
-  chrome-devtools = {
-    type = "stdio";
-    command = npx;
-    args = [
-      "chrome-devtools-mcp@latest"
-      "--executablePath=${pkgs.google-chrome}/bin/google-chrome-stable"
-    ];
-    env = { };
-  };
   codex = {
     type = "stdio";
     command = codex;
@@ -35,6 +27,17 @@ in
     command = "${pkgs.github-mcp-server}/bin/github-mcp-server";
     args = [
       "stdio"
+    ];
+    env = { };
+  };
+}
+// lib.optionalAttrs (pkgs.stdenv.hostPlatform.system != "aarch64-linux") {
+  chrome-devtools = {
+    type = "stdio";
+    command = npx;
+    args = [
+      "chrome-devtools-mcp@latest"
+      "--executablePath=${pkgs.google-chrome}/bin/google-chrome-stable"
     ];
     env = { };
   };
