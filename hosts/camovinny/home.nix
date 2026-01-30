28a2d4835cecd5282e2784854ce9458b514c9a1f
Vinny Meller <vinnymeller@proton.me>
2026-01-30 04:25:14 -0600

    add pkgs to user level

--- hosts/camovinny/home.nix
+++ hosts/camovinny/home.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 {
@@ -13,5 +14,16 @@
   home.username = "vinny";
   home.homeDirectory = "/Users/vinny";
   home.stateVersion = "22.11";
+
+  home.packages = with pkgs; [
+    awscli2
+    ijq
+    jq
+    sqlfluff
+    nodejs_22
+    (pre-commit.override { dotnet-sdk = pkgs.stable-pkgs.dotnet-sdk; })
+    uv
+    (dbt.withAdapters (a: [ a.dbt-snowflake ]))
+  ];
 
 }
