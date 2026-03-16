b0c43be8373bb284b0e85ed4e32a0bc719f924ce
Vinny Meller <vinnymeller@proton.me>
2026-03-16 11:00:14 -0500

    remove old immich module & rename docker one

--- modules/nixos/immich.nix
+++ modules/nixos/immich.nix
@@ -1,59 +1,104 @@
 {
-  config,
   lib,
+  config,
   ...
 }:
+
 let
-  inherit (lib)
-    mkEnableOption
-    mkIf
-    mkOption
-    types
-    ;
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.mine.services.immich;
+  mediaDir = "/data/immich";
+  dbDir = "/var/lib/immich/postgres";
 in
 {
-
-  options.mine.services.immich = {
-    enable = mkEnableOption "Enable Immich";
-    user = mkOption {
-      type = types.str;
-      default = "immich";
-      description = "User to run Immich under.";
-    };
-    group = mkOption {
-      type = types.str;
-      default = "immich";
-      description = "Group to run Immich under.";
-    };
-  };
+  options.mine.services.immich.enable = mkEnableOption "Immich media server";
 
   config = mkIf cfg.enable {
+    mine.services.dockerCompose.stacks.immich = {
+      gpu.enable = true;
+      autoUpdate.enable = false;
+      agenix.envFile = {
+        file = ../../secrets/vinnix/immich.age;
+        services = [
+          "immich-server"
+          "database"
+        ];
+      };
 
-    age.secrets.immich = {
-      file = ../../secrets/vinnix/immich.age;
-      owner = cfg.user;
-      group = cfg.group;
-      mode = "0400";
-    };
+      compose = {
+        services = {
+          immich-server = {
+            image = "ghcr.io/immich-app/immich-server:release";
+            container_name = "immich_server";
+            restart = "always";
+            ports = [ "2283:2283" ];
+            environment = {
+              DB_HOSTNAME = "database";
+              DB_USERNAME = "immich";
+              DB_DATABASE_NAME = "immich";
+              REDIS_HOSTNAME = "redis";
+            };
+            volumes = [
+              "${mediaDir}:/data"
+              "/etc/localtime:/etc/localtime:ro"
+            ];
+            depends_on = [
+              "redis"
+              "database"
+            ];
+          };
 
-    services.immich = {
-      enable = true;
-      user = cfg.user;
-      group = cfg.group;
-      accelerationDevices = null; # null gives access to all devices
-      secretsFile = config.age.secrets.immich.path;
-      host = "0.0.0.0";
+          immich-machine-learning = {
+            image = "ghcr.io/immich-app/immich-machine-learning:release-cuda";
+            container_name = "immich_machine_learning";
+            restart = "always";
+            volumes = [ "model-cache:/cache" ];
+          };
+
+          redis = {
+            image = "docker.io/valkey/valkey:9";
+            container_name = "immich_redis";
+            restart = "always";
+            healthcheck = {
+              test = [
+                "CMD"
+                "redis-cli"
+                "ping"
+              ];
+            };
+          };
+
+          database = {
+            image = "ghcr.io/immich-app/postgres:18-vectorchord0.5.3-pgvector0.8.1";
+            container_name = "immich_postgres";
+            restart = "always";
+            environment = {
+              POSTGRES_USER = "immich";
+              POSTGRES_DB = "immich";
+              POSTGRES_INITDB_ARGS = "--data-checksums";
+            };
+            volumes = [
+              "${dbDir}:/var/lib/postgresql"
+            ];
+            shm_size = "128mb";
+          };
+        };
+
+        volumes.model-cache = { };
+      };
     };
 
     mine.services.restic.jobs.immich = {
-      paths = [ config.services.immich.mediaLocation ];
+      paths = [ mediaDir ];
+      exclude = [
+        "${mediaDir}/thumbs"
+        "${mediaDir}/encoded-video"
+      ];
+      extraBackupArgs = [ "--verbose" ];
     };
 
-    users.users.${cfg.user}.extraGroups = [
-      "video"
-      "render"
+    systemd.tmpfiles.rules = [
+      "d ${dbDir} 0755 root root -"
     ];
-
   };
 }
