f627d266f073e522cddb0c714ab8ebfff8bc7d14
Vinny Meller <vinnymeller@proton.me>
2026-03-20 05:46:41 -0500

    add 'apps'

--- hosts/vinnix/users/vinny/home.nix
+++ hosts/vinnix/users/vinny/home.nix
@@ -20,39 +20,53 @@
     defaultCacheTtlSsh = 10;
   };
 
-  home.packages =
-    with pkgs;
-    [
-      feh
-      gcc
-      discord-canary
-      gocryptfs
-      htop
-      libvirt
-      lxsession
-      man-pages # linux man pages, goes with tlpi
-      fastfetch
-      nerd-fonts._0xproto
-      nix-init
-      obs-studio
-      (master-pkgs.osu-lazer.override { nativeWayland = true; })
-      pavucontrol
-      pkg-config
-      protonup-qt
-      qbittorrent-enhanced
-      qemu
-      sbctl
-      screenkey
-      virt-manager
-      yubioath-flutter
-      yubikey-manager
-      zsh-powerlevel10k
-    ];
+  home.packages = with pkgs; [
+    feh
+    gcc
+    discord-canary
+    gocryptfs
+    htop
+    libvirt
+    lxsession
+    man-pages # linux man pages, goes with tlpi
+    fastfetch
+    nerd-fonts._0xproto
+    nix-init
+    obs-studio
+    (master-pkgs.osu-lazer.override { nativeWayland = true; })
+    pavucontrol
+    pkg-config
+    protonup-qt
+    qbittorrent-enhanced
+    qemu
+    sbctl
+    screenkey
+    virt-manager
+    yubioath-flutter
+    yubikey-manager
+    zsh-powerlevel10k
+  ];
 
   programs.vesktop = {
     enable = true;
     vencord.useSystem = true;
   };
+
+  xdg.desktopEntries = {
+    jellyfin = {
+      name = "Jellyfin";
+      exec = "${pkgs.google-chrome}/bin/google-chrome-stable --app=https://jellyfin.vinnix.net";
+      icon = "jellyfin";
+      type = "Application";
+    };
+    grimmory = {
+      name = "Grimmory";
+      exec = "${pkgs.google-chrome}/bin/google-chrome-stable --app=https://grimmory.vinnix.net";
+      icon = "booklore";
+      type = "Application";
+    };
+  };
+
   programs.spotify-player.enable = true;
 
   programs.command-not-found.enable = false;
